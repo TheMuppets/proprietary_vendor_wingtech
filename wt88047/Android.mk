7db85191a965a7088a70ce5c8846d23c9be43539
Nicholas Lim <nicknitewolf@lineageos.org>
2020-06-24 22:13:11 +0800

    wt88047: Drop time service blobs except stock libtime_genoff.so
     * All of a sudden, time_genoff connection will fail and sometimes
       block unlocking with password, etc. Stock libtime_genoff.so will
       fix it in most cases, but not all. Drop time service stack to make
       this connection fail at start to workaround the problem. Time
       service is broken on 10 anyway so it is a good trade-off.

--- wt88047/Android.mk
+++ wt88047/Android.mk
@@ -99,18 +99,6 @@ LOCAL_MODULE_SUFFIX := .apk
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := TimeService
-LOCAL_MODULE_OWNER := wingtech
-LOCAL_SRC_FILES := proprietary/vendor/app/TimeService/TimeService.apk
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := APPS
-LOCAL_DEX_PREOPT := false
-LOCAL_PROPRIETARY_MODULE := true
-LOCAL_MODULE_SUFFIX := .apk
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := qcrilmsgtunnel
 LOCAL_MODULE_OWNER := wingtech
 LOCAL_SRC_FILES := proprietary/priv-app/qcrilmsgtunnel/qcrilmsgtunnel.apk
